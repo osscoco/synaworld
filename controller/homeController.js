43a073e8db505afb863353c0f60461af149a8d28
Corentin <corentin.jeannot2a@gmail.com>
2022-11-08 13:44:40 +0100

    add checking security_2

--- controller/homeController.js
+++ controller/homeController.js
@@ -4,6 +4,14 @@ const getHomePage = (req, res) => {
 
 }
 
+const getSecurePage = (req, res) => {
+
+    fs.readFile("05be2a19091bd3b9072e59a3fbd344b7.txt","utf8",function(err,html) {
+        res.send(html);
+    });
+}
+
 module.exports = {
-    getHomePage
+    getHomePage,
+    getSecurePage
 };
